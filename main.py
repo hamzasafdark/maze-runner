226046a24ff1b538315d0dd9ef8317c29209026a
Hamza Khan <hamzasafdark@gmail.com>
2024-02-17 01:23:12 +0500

    draw cells and walls

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
-from graphics import Window, Line, Point
+from graphics import Window
+from cell import Cell
 
 
 def main():
     win = Window(800, 600)
-    l1 = Line(Point(50, 50), Point(400, 400))
-    l2 = Line(Point(400, 10), Point(10, 200))
-    win.draw_line(l1, "black")
-    win.draw_line(l2, "green")
+
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50,50,100,100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
+
     win.wait_for_close()
 
 main()
